cd6ae963cc36fdd8bd9df3c06cf843770053046b
Mtho <mthontanzi99@gmail.com>
2024-11-10 08:51:29 +0200

    Add routers to the different pages

---
+++ thabo-portfolio/src/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="navBar">
+        <router-link to="/">Online Portfolio</router-link>
+        <router-link to="/about">About Me</router-link>
+        <router-link to="/education">Education</router-link>
+        <router-link to="/experience">Experience</router-link>
+        <router-link to="/project">Project</router-link>
+        <router-link to="/contact">Contact</router-link>
+    </div>
+</template>
--- thabo-portfolio/src/router/index.js
+++ thabo-portfolio/src/router/index.js
@@ -1,20 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import AboutMe from '../views/AboutMe.vue'
+import Education from '../views/Education.vue'
+import Experience from '../views/Experience.vue'
+import Project from '../views/Project.vue'
+import Contact from '../views/Contact.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    {path: '/', name: 'home', component: Home},
+    {path: '/about', name: 'about', component: AboutMe},
+    {path: '/education', education: 'home', component: Education},
+    {path: '/experience', name: 'experience', component: Experience},
+    {path: '/project', name: 'project', component: Project},
+    {path: '/contact', name: 'contact', component: Contact}
 ]
 
 const router = createRouter({
